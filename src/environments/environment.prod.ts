345803f5a63f501cf34fd80ac98a0ec13fab9915
Slimane Deb <cs_deb@esi.dz>
2019-01-30 15:58:52 +0100

    final changes for v1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: 'AIzaSyCEiYapBqIUF_-ppZOdjC3ZqZ1bzr-91rw',
+    authDomain: 'oshop-eed64.firebaseapp.com',
+    databaseURL: 'https://oshop-eed64.firebaseio.com',
+    projectId: 'oshop-eed64',
+    storageBucket: 'oshop-eed64.appspot.com',
+    messagingSenderId: '994225241922'
   }
 };
